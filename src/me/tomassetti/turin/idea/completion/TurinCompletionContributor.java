b4f5afce1a9e7b869b22aadd719b6839590e340d
Federico Tomassetti <federico@tomassetti.me>
2015-09-23 22:19:28 +0200

    initial code completion

--- src/me/tomassetti/turin/idea/completion/TurinCompletionContributor.java
+++ src/me/tomassetti/turin/idea/completion/TurinCompletionContributor.java
@@ -3,6 +3,8 @@ package me.tomassetti.turin.idea.completion;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.tree.IElementType;
@@ -12,44 +14,163 @@ import me.tomassetti.turin.idea.TurinLanguage;
 import me.tomassetti.turin.idea.antlradaptor.lexer.RuleElementType;
 import me.tomassetti.turin.idea.antlradaptor.lexer.TokenElementType;
 import me.tomassetti.turin.idea.parser.TurinTokenTypes;
-import me.tomassetti.turin.implicit.BasicTypeUsage;
-import me.tomassetti.turin.parser.ast.typeusage.PrimitiveTypeUsage;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class TurinCompletionContributor extends CompletionContributor {
-    @Override
-    public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
-        System.out.println("FEDERICO ON " + parameters);
-        //IElementType elementType = parameters.getPosition().getNode().getElementType();
-        IElementType elementType = parameters.getOriginalPosition().getNode().getElementType();
-        PsiElement psiElement = parameters.getOriginalPosition().getOriginalElement();
-        System.out.println("FEDERICO ON1 " + elementType);
-        System.out.println("FEDERICO ON2 " + elementType.getIndex());
-        System.out.println("FEDERICO ONPSI " + psiElement);
-        System.out.println("FEDERICO ONPSI2 " + psiElement.getNode());
-        System.out.println("FEDERICO ONPSI2a " + psiElement.getNode().getTreeParent());
-        System.out.println("FEDERICO ONPSI2b " + psiElement.getNode().getTreeParent().getTreeParent());
-        System.out.println("FEDERICO ONPSI3 " + psiElement.getText());
+
+    public boolean isNl(ASTNode node) {
+        if (node == null) {
+            return false;
+        }
+        if (node.getElementType() == null) {
+            return false;
+        }
+        return node.getElementType().getIndex() == TurinTokenTypes.TOKEN_ELEMENT_TYPES.get(TurinParser.NL).getIndex();
+    }
+
+    public boolean isWs(ASTNode node) {
+        if (node == null) {
+            return false;
+        }
+        if (node.getElementType() == null) {
+            return false;
+        }
+        return node.getElementType().getIndex() == TurinTokenTypes.TOKEN_ELEMENT_TYPES.get(TurinParser.WS).getIndex();
+    }
+
+    public boolean isNlWs(ASTNode node) {
+        return isNl(node) || isWs(node);
+    }
+
+    public boolean isRule(ASTNode node, int rule) {
+        if (node == null) {
+            return false;
+        }
+        if (node.getElementType() == null) {
+            return false;
+        }
+        return node.getElementType().getIndex() == TurinTokenTypes.RULE_ELEMENT_TYPES.get(rule).getIndex();
+    }
+
+    private enum Place {
+        STATEMENT_CONTAINER, FILE_MEMBER
+    }
+
+    private void explore(ASTNode node, int level, TextRange textRange, Set<Place> places) {
+        // treat NL or WS in a special way (look for successors nodes)
+        if (level == 0 && isNl(node)) {
+            ASTNode end = node;
+            while (isNlWs(end.getTreeNext())) {
+                end = end.getTreeNext();
+            }
+            textRange = end.getTextRange();
+        }
+
+        System.out.println("FEDERICO (level + " + level+") " + node + " starting at " + node.getStartOffset() + " ending at " + node.getTextRange().getEndOffset());
+        boolean sameStart = node.getTextRange().getStartOffset() == textRange.getStartOffset();
+        boolean sameEnd = node.getTextRange().getEndOffset() == textRange.getEndOffset();
+        boolean inside = !sameStart && !sameEnd;
+        if (sameStart) {
+            System.out.println("  FEDERICO same start");
+            if (isRule(node, TurinParser.RULE_fileMember)) {
+                places.add(Place.FILE_MEMBER);
+            }
+        }
+        if (sameEnd) {
+            System.out.println("  FEDERICO same end");
+            if (isRule(node, TurinParser.RULE_fileMember)) {
+                places.add(Place.FILE_MEMBER);
+            }
+        }
+        if (inside) {
+            System.out.println("  FEDERICO same inside");
+            if (isRule(node, TurinParser.RULE_methodBody)) {
+                places.add(Place.STATEMENT_CONTAINER);
+            }
+        }
+
+        IElementType elementType = node.getElementType();
         for (RuleElementType ruleElementType :TurinTokenTypes.RULE_ELEMENT_TYPES) {
             if (ruleElementType.getIndex() == elementType.getIndex()) {
                 int ruleIndex = ruleElementType.getRuleIndex();
-                System.out.println("FEDERICO ON3 " + ruleIndex);
+                System.out.println("  FEDERICO -> it is a RE " + ruleIndex);
             }
         }
         for (TokenElementType ruleElementType :TurinTokenTypes.TOKEN_ELEMENT_TYPES) {
             if (ruleElementType.getIndex() == elementType.getIndex()) {
                 int ruleIndex = ruleElementType.getType();
-                System.out.println("FEDERICO ON4 " + ruleIndex);
+                System.out.println("  FEDERICO -> it is a TE " + ruleIndex);
             }
         }
-        result.addElement(new LookupElement() {
-            @NotNull
-            @Override
-            public String getLookupString() {
-                return "A";
-            }
-        });
+
+        if (node.getTreeParent() != null) {
+            explore(node.getTreeParent(), level + 1, textRange, places);
+        }
+    }
+
+    @Override
+    public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+        System.out.println("\n=======================\n");
+        System.out.println("FEDERICO ON " + parameters);
+        //IElementType elementType = parameters.getPosition().getNode().getElementType();
+        IElementType elementType = parameters.getOriginalPosition().getNode().getElementType();
+        PsiElement psiElement = parameters.getOriginalPosition().getOriginalElement();
+        System.out.println("FEDERICO start at " + psiElement.getTextRange().getStartOffset() + " end at " + psiElement.getTextRange().getEndOffset());
+        Set<Place> places = new HashSet<Place>();
+        explore(psiElement.getNode(), 0, parameters.getOriginalPosition().getNode().getTextRange(), places);
+
+        if (places.contains(Place.FILE_MEMBER)) {
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "type NewType {\n}\n";
+                }
+            });
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "program MyProgram (String[] args) {\n}\n";
+                }
+            });
+        }
+        if (places.contains(Place.STATEMENT_CONTAINER)) {
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "val a = 0\n";
+                }
+            });
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "if true {\n}\n";
+                }
+            });
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "return 0\n";
+                }
+            });
+            result.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+                    return "try {\n" +
+                            "} catch Exception e {\n" +
+                            "}\n";
+                }
+            });
+        }
     }
 
     public TurinCompletionContributor() {
